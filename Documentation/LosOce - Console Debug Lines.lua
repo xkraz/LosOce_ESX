84b7a5801a1664c1416c6e0c32585b8f42c73528
DK-DonnieKongy <52999643+DK-DonnieKongy@users.noreply.github.com>
2019-08-08 20:48:22 +1000

    Update LosOce - Console Debug Lines.lua

--- Documentation/LosOce - Console Debug Lines.lua
+++ Documentation/LosOce - Console Debug Lines.lua
@@ -1,3 +1,8 @@
-print('	^0[^1Alert^0] : | . | : [^1Alert^0] ')	
-print('	^0[^3Error^0] : | . | : [^3Error^0] ')	
-print('	^0[^5Debug^0] : | . | : [^5Debug^0]')	
+-- Alerts
+print('	^0[^1Alert^0] (Funciton/Event/Tread) : NAMEOF |^1 DESCRIPTION^0. | NAMEOF : (Funciton/Event/Tread) ^0[^1Alert^0] ')
+
+-- Errors
+print('	^0[^3Error^0] (Funciton/Event/Tread) : NAMEOF |^3 DESCRIPTION^0. | NAMEOF : (Funciton/Event/Tread) ^0[^3Error^0] ')
+
+-- Debugging
+print('	^0[^5Debug^0] (Funciton/Event/Tread) : NAMEOF |^5 DESCRIPTION^0. | NAMEOF : (Funciton/Event/Tread) ^0[^5Debug^0] ')	
